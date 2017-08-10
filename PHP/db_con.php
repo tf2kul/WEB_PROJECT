32143f89b746afd4335eaaed53ae123e777ba91c
alstjr4192 <alstjrskdh@naver.com>
2017-08-11 01:22:45 +0900

    Update db_con.php

--- PHP/db_con.php
+++ PHP/db_con.php
@@ -1,15 +1,10 @@
 <?php
 $dsn = "mysql:host=localhost;port=3306;dbname=web_project;charset=utf8";
 try {
-    $db = new PDO($dsn,"admin","admin"); //db 연결
+    $db = new PDO($dsn,"root","123123"); //db 연결
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //db기능 실행못할때 실행할수있게 해줌
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //에러를 처리해줌
     
-    $query = "SELECT id,pw from WEB_PROJECT";
-    $last = $db->prepare($query);
-    $last->execute();
-    
-    $query_result = $last->fetch(PDO::FETCH_ASSOC);
 }
     
     
