8a3868d60f862b8f4c4567e22ff19505b8734bc3
alstjr4192 <alstjrskdh@naver.com>
2017-08-11 12:48:49 +0900

    Update singup.php

--- PHP/singup.php
+++ PHP/singup.php
@@ -1,34 +1,6 @@
-<?php
-include('db_con.php');
-
-$id = $_POST['id'];
-$pw = $_POST['pw'];
-$name = $_POST['name'];
-$re_pw = $_POST['re_pw'];
-
-if(empty($id) || empty($pw)||empty($name)||empty($re_pw)) {
-    echo "전부 다 입력하세요 구르트^^<br>";
-    exit;
-}
-
-else if( $pw!=$re_pw ) {
-	echo "두개의 비밀번호 입력값이 다릅니다 람쥐 고기";
-	exit;
-}
-else {
-$query = "SELECT * from user";
-$last = $db->query($query);
-$last->setFetchMode(PDO::FETCH_ASSOC);
-while($row= $last->fetch()){
-    if($id==$row['id']) {
-	echo "똑같은 아이디가 존재합니다";
-	exit();
-   }
- }
-}
-
-$hash_pw = hash('sha512',$pw);
-$query = "INSERT INTO user VALUES('$id','$hash_pw','$re_pw','$name')";
-$signup = $db->exec($query);
-
+<?phpinclude('db_con.php');
+$id = $_POST['id'];$pw = $_POST['pw'];$name = $_POST['name'];$re_pw = $_POST['re_pw'];$email = $_POST['email'];
+if(empty($id) || empty($pw)||empty($name)||empty($re_pw) || empty($email)) {    echo "전부 다 입력하세요^^<br>";    exit;}
+else if( $pw!=$re_pw ) { echo "두개의 비밀번호 입력값이 다릅니다"; exit;}else {$query = "SELECT * from user";$last = $db->query($query);$last->setFetchMode(PDO::FETCH_ASSOC);while($row= $last->fetch()){    if($id==$row['id'] || $email==$row['email']) { echo "똑같은 아이디가 존재합니다"; exit();   } }}
+$hash_pw = hash('sha512',$pw);//$query = "INSERT into user(id,pw,email,name) VALUES('admin','admin','admin@naver.com','admin')";$query = "INSERT INTO user(id,pw,email,name) VALUES('$id','$hash_pw','$name','$email')";$signup = $db->exec($query);
 ?>
