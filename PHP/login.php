faa58f17c5c5e4ecb2329bfd79be0d30bf357d6e
alstjr4192 <alstjrskdh@naver.com>
2017-08-11 12:48:26 +0900

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -1,39 +1,7 @@
 <?php
-
-include('db_con.php');
-session_start();
-$good_login = 0;
-$id = $_POST['id'];
-$pw = $_POST['pw'];
-
-if(empty($id) || empty($pw)) {
-    echo "아이디나 비밀번호를 치세요<br>";
-    exit;
-}
-$hash_pw = hash('sha512',$pw);
-$query = "SELECT * from user";
-//$query = "SELECT * from login WHERE id==$id and pw==$pw";
-$last = $db->query($query);
-$last->setFetchMode(PDO::FETCH_ASSOC);
-
-while($row= $last->fetch()){
-if($id == $row['id'] && $hash_pw == $row['pw']) {
-	$good_login = 1;
-	break;
-	}
-}
-if($good_login) {
-	echo "Login Success !";
-	exit;
-}
-else {
-	echo "G00d..bye..";
-	exit;
-}
+include('db_con.php');session_start();$good_login = 0;$id = $_POST['id'];$pw = $_POST['pw'];
+if(empty($id) || empty($pw)) {    echo "아이디나 비밀번호를 치세요<br>";    exit;}$hash_pw = hash('sha512',$pw);$query = "SELECT * from user";//$query = "SELECT * from login WHERE id==$id and pw==$pw";$last = $db->query($query);$last->setFetchMode(PDO::FETCH_ASSOC);
+while($row= $last->fetch()){if($id == $row['id'] && $hash_pw == $row['pw']) { $good_login = 1; break; }}if($good_login) { echo "Login Success !";    $_SESSION['id'] = $id; exit;}else { echo "G00d..bye.."; exit;}
 
 
 
-
-
-
-?>
